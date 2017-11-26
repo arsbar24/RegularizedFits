ff5d0cd70c3b18868bbc9d2ba093dd073d80ee57
Alistair Barton <alistair.barton@mail.mcgill.ca>
2017-11-26 02:57:15 -0800

    Initialized

---
+++ tests/testthat/test_regfit.R
@@ -0,0 +1,25 @@
+context("Comparing reg.lm with lm")
+
+test_that("Does regularized match linear model when lambda = 0", {
+	x <- c(1, 5.4, 2.1)
+	y <- c(0.2, -3, 7.1)
+	reg <- reg.lm(y,x,lambda = 0)
+	lm <- lm(y~x)
+	expect_equal(as.numeric(lm$coefficients), as.numeric(reg$coefficients))
+})
+
+test_that("Are they the same when lambda != 0", {
+	x <- c(1, 5.4, 2.1)
+	y <- c(0.2, -3, 7.1)
+	reg <- reg.lm(y,x,lambda = 0.1)
+	lm <- lm(y~x)
+	expect_equal(as.numeric(lm$coefficients), as.numeric(reg$coefficients))
+})
+
+test_that("Does fit have positive slope when data positively correlated?", {
+	x <- 1:5
+	y <- (1:5)^3
+	reg <- reg.lm(y,x,lambda = 1)
+	expect_true(reg$coefficients[2]>0)
+})
+
